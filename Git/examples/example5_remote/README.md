5e351777bf25aaea779ba759f67bfce654795bd3
Atisam ul haq <38161450+atisamhaq123@users.noreply.github.com>
2024-04-15 17:49:49 +0500

    Add files via upload

---
+++ Git/examples/example5_remote/README.md
@@ -0,0 +1,7 @@
+# Train_Class..
+
+
+<img align="right" alt="Coding" src="https://github.com/jawadsamiulhaq/Train_Class/blob/main/Train_Class.PNG">
+
+
+
---
+++ Git/examples/example5_remote/Train_Class.py
@@ -0,0 +1,36 @@
+class Train():
+    def __init__(self,name,fare,seats):
+        self.name = name 
+        self.fare = fare 
+        self.seats = seats 
+    def getFare(self):
+        print(f"The Fare Of Train Is {self.fare}   ")
+    def getStatus(self):
+        print("--------------Train Info------------")
+        print(f"Name Of Train Is {self.name}")
+        print(f"Total Number Of Seats Available In {self.name} Is {self.seats}")
+    def bookTicket(self):
+        if(self.seats>0):
+            print("-----------------------------------")
+            print(f"You Ticket Have Book! And YOu Seat Number Is {self.seats}")
+            self.seats = self.seats - 1
+        else:
+            print("-------------------------------------")
+            print("Sorry! No More Tickets Are Availablee")
+    def cancelTicket(self):
+        self.seats = self.seats + 1
+        
+        print(f"You Cancell YOur Ticket! {self.seatss}")
+<<<<<<< HEAD
+intercity = Train("Hazara Express",100,90)
+=======
+intercity = Train("Hazara Express",100,90)
+>>>>>>> 7cd6060987729a9c933d1b162163dd217f2ea0e8
+intercity.getStatus()
+intercity.bookTicket()
+intercity.getStatus()
+intercity.cancelTicket()
+intercity.getStatus()
+
+
+
